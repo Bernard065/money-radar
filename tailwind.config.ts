7dfd0efeef44e98b70bbc40eae047352fe2c51ee
Bernard065 <bebenibernard@gmail.com>
2024-11-17 16:15:04 +0300

    setup fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#FFF1E6",
+          500: "#FF7000",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0F1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#101012",
+        },
+        light: {
+          900: "#FFFFFF",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
+      },
+      screens: {
+        xs: "420px",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
       },
     },
   },
