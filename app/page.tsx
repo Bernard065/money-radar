a2701a2919e5c589e9f02c1ee3effe6b323637e9
Bernard065 <bebenibernard@gmail.com>
2024-11-17 10:59:56 +0300

    set up ESLint and  Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="size-full flex fle-col gap-6 items-center justify-center">
+    <div className=" flex size-full  flex-col items-center justify-center gap-6">
       <h1>Home</h1>
       <Link href="/about">About</Link>
     </div>
